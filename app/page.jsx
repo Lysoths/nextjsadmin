cedaa0372ce6dff6c2779c9e9e38afe7f04a7fe5
Oğuzhan ER <ogzhner@hotmail.com>
2023-11-21 19:34:01 +0300

    .

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div>
+      <Link href={`/dashboard/`}>
+        <p className="goto">Go to dashboard...</p>
+      </Link>
+    </div>
+  );
 };
 
 export default Page;
